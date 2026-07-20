52f43c240e4d7362450229beb8c56164ffb6d1ef
AlejandroFelipe Acuna <afacunam@gmail.com>
2026-07-20 12:40:57 +0200

    feat: Challenge 1 ok

--- src/basics-refactor.test.ts
+++ src/basics-refactor.test.ts
@@ -10,7 +10,7 @@ Llegeix l'article:
 
 describe("Problema de nombres", () => {
   it("Ha de sumar els dos nombres", () => {
-    const addTwoNumbers = (a, b) => {
+    const addTwoNumbers = (a: number, b: number): number => {
       return a + b;
     };
     expectTypeOf(addTwoNumbers).parameter(0).toEqualTypeOf<number>();
@@ -23,29 +23,37 @@ Repte 2:
 Descobreix com tipar params com un objecte amb una clau first que sigui un nombre i una clau second que també sigui un nombre.
 */
 
-// describe("Problema de paràmetre objecte", () => {
-//   it("Ha de sumar els dos nombres", () => {
-//     const addTwoNumbers = (params:{first:number,second:number}) => {
-//       return params.first + params.second;
-//     };
-//     expect(
-//       addTwoNumbers({
-//         first: 2,
-//         second: 4,
-//       })
-//     ).toEqual(6);
+interface Param {
+  first: number;
+  second: number;
+};
 
-//     expect(
-//       addTwoNumbers({
-//         first: 10,
-//         second: 20,
-//       })
-//     ).toEqual(30);
-//   });
-//   expectTypeOf(addTwoNumbers).parameter(0).toEqualTypeOf<{ first: number; second: number }>();
+ describe("Problema de paràmetre objecte", () => {
+   it("Ha de sumar els dos nombres", () => {
+     const addTwoNumbers = (params: Param) => {
+       return params.first + params.second;
+     };
+     expect(
+       addTwoNumbers({
+         first: 2,
+         second: 4,
+       })
+     ).toEqual(6);
+
+     expect(
+       addTwoNumbers({
+         first: 10,
+         second: 20,
+       })
+     ).toEqual(30);
 
 
-// });
+     
+   });
+
+  expectTypeOf(addTwoNumbers).parameter(0).toEqualTypeOf<{ first: number; second: number }>();
+
+ });
 
 // /*
 // Repte 3:
