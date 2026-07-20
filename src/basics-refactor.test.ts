1cf179fcdc1f4488d7ce92acd7853175941d9443
AlejandroFelipe Acuna <afacunam@gmail.com>
2026-07-20 16:29:28 +0200

    Challenge 6 ok

--- src/basics-refactor.test.ts
+++ src/basics-refactor.test.ts
@@ -159,28 +159,28 @@ interface Param {
 // Actualitza la interfície User per restringir la propietat role a una de les opcions establertes.
 // El valor I_SHOULD_NOT_BE_ALLOWED hauria de provocar un error, eliminant la línia vermella sota el comentari // @ts-expect-error.
 // */
-// describe("Problema d'unions", () => {
-//   interface User {
-//     id: number;
-//     firstName: string;
-//     lastName: string;
-//     /**
+ describe("Problema d'unions", () => {
+   interface User {
+     id: number;
+     firstName: string;
+     lastName: string;
+     /**
 //      * Com ens assegurem que role només sigui un dels següents:
 //      * - 'admin'
 //      * - 'user'
 //      * - 'super-admin'
 //      */
-//     role: string;
-//   }
+     role: 'admin' | 'user' | 'super-admin';
+   }
 
-//   const defaultUser: User = {
-//     id: 1,
-//     firstName: "Jen",
-//     lastName: "Simmons",
-//     // @ts-expect-error
-//     role: "I_SHOULD_NOT_BE_ALLOWED",
-//   };
-// });
+   const defaultUser: User = {
+     id: 1,
+     firstName: "Jen",
+     lastName: "Simmons",
+     // @ts-expect-error
+     role: "I_SHOULD_NOT_BE_ALLOWED",
+   };
+ });
 
 // /*
 // Repte 7:
