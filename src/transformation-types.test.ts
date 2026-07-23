a52f14411b666121f87593817742ee380896b86a
AlejandroFelipe Acuna <afacunam@gmail.com>
2026-07-23 12:06:02 +0200

    Transformation Challenge 7 ok

--- src/transformation-types.test.ts
+++ src/transformation-types.test.ts
@@ -145,17 +145,15 @@ import { Equal, Expect } from "./helpers/type-utils";
 // Repte 7:
 // Utilitza indexed access types i unions per obtenir el tipus dels valors d'un array.
 // */
-// describe("Transformació: obtenir el tipus dels valors d'un array", () => {
-//   const fruits = ["apple", "banana", "orange"];
-
-//   type AppleOrBanana = unknown;
-//   type Fruit = unknown;
-
-//   type tests = [
-//     Expect<Equal<AppleOrBanana, "apple" | "banana">>,
-//     Expect<Equal<Fruit, "apple" | "banana" | "orange">>,
-//   ];
-// });
+ describe("Transformació: obtenir el tipus dels valors d'un array", () => {
+   const fruits = ["apple", "banana", "orange"] as const;
+   type AppleOrBanana = typeof fruits[0 | 1];
+   type Fruit = typeof fruits[number];
+   type tests = [
+     Expect<Equal<AppleOrBanana, "apple" | "banana">>,
+     Expect<Equal<Fruit, "apple" | "banana" | "orange">>,
+   ];
+ });
 
 // /*
 // Repte 8:
